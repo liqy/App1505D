eee1c4b0bcb8f789503f0f1a2009c660cecdd3b3
liqy <liqingyi@outlook.com>
2017-08-02 10:46:05 +0800

    功能1开发完成

--- app/src/main/java/com/bwie/app1505d/MainActivity.java
+++ app/src/main/java/com/bwie/app1505d/MainActivity.java
@@ -1,13 +1,30 @@
 package com.bwie.app1505d;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+/**
+ * 主界面
+ */
 public class MainActivity extends AppCompatActivity {
+
+    TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        feature1();
+
+    }
+
+    /**
+     * 这是第一个功能
+     */
+    private void feature1() {
+        hello = (TextView) findViewById(R.id.hello);
+        hello.setText("这是第一个功能");
     }
 }
